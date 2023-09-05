b0d2482c4340143378c7b456ca9f77bd2abfe621
Andy | アンディ <ayamgorengmanis87@gmail.com>
2023-09-05 10:56:40 +0800

    stone: Clone dependencies using vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+git clone --depth 1 https://github.com/moonstone-devs/device_xiaomi_sm6375-common device/xiaomi/sm6375-common
+
+git clone --depth 1 https://github.com/Xiaomi-SM6375/proprietary_vendor_xiaomi vendor/xiaomi
+
+git clone --depth 1 https://github.com/xiaomi-sm6375-devs/android_kernel_xiaomi_sm6375 kernel/xiaomi/sm6375
+
+git clone --depth 1 https://github.com/Xiaomi-SM6375/device_xiaomi_stone-kernel device/xiaomi/stone-kernel
+
+git clone --depth 1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
