f010f5185fc835cc3c85e3602a313d2621661030
Andy | アンディ <ayamgorengmanis87@gmail.com>
2023-09-07 23:41:36 +0800

    stone: vendorsetup: Tracking our remote for some repository

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,9 @@
-git clone --depth 1 https://github.com/moonstone-devs/device_xiaomi_sm6375-common device/xiaomi/sm6375-common
+git clone --depth 1 https://github.com/stone-devs/device_xiaomi_sm6375-common device/xiaomi/sm6375-common
 
-git clone --depth 1 https://github.com/Xiaomi-SM6375/proprietary_vendor_xiaomi vendor/xiaomi
+git clone --depth 1 https://github.com/stone-devs/proprietary_vendor_xiaomi vendor/xiaomi
 
-git clone --depth 1 https://github.com/xiaomi-sm6375-devs/android_kernel_xiaomi_sm6375 kernel/xiaomi/sm6375
+git clone --depth 1 https://github.com/stone-devs/kernel_xiaomi_sm6375 kernel/xiaomi/sm6375
 
-git clone --depth 1 https://github.com/Xiaomi-SM6375/device_xiaomi_stone-kernel device/xiaomi/stone-kernel
+git clone --depth 1 https://github.com/stone-devs/device_xiaomi_stone-kernel device/xiaomi/stone-kernel
 
-git clone --depth 1 https://github.com/moonstone-devs/hardware_xiaomi hardware/xiaomi
+git clone --depth 1 https://github.com/stone-devs/hardware_xiaomi hardware/xiaomi
