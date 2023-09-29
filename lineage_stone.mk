3b5281286507fc252e58dd9e683acbd9e1fc5600
Drpto <dr.juniornoob@gmail.com>
2023-09-29 22:42:36 +0530

    stone: Add FaceUnlock support for A13 roms
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- lineage_stone.mk
+++ lineage_stone.mk
@@ -17,6 +17,7 @@ $(call inherit-product, device/xiaomi/stone/device.mk)
 # Device props
 TARGET_SUPPORTS_BLUR := true
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Charging Animation
 TARGET_USE_PIXEL_CHARGER := true
