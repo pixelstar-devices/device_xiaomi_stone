3c2f9f925e6d21094aab3136947113a7c920b7b2
Drpto <dr.juniornoob@gmail.com>
2023-10-02 00:18:06 +0530

    stone: track GCamGo as prebuilt camera from https://github.com/Drpto/packages_apps_prebuilts.git
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,3 +7,5 @@ git clone --depth=1 https://github.com/Stone-Trees/kernel_xiaomi_sm6375.git -b l
 git clone --depth=1 https://github.com/Stone-Trees/xiaomi_stone-kernel.git device/xiaomi/stone-kernel
 
 git clone --depth=1 https://github.com/stone-devs/hardware_xiaomi hardware/xiaomi
+
+git clone --depth=1 https://github.com/Drpto/packages_apps_prebuilts.git packages/apps/prebuilts
