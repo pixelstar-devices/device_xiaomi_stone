23b79f057545982dd92261edc19e2a96677d1e89
Drpto <dr.juniornoob@gmail.com>
2023-11-20 23:20:10 +0530

    stone: Track hardware_xiaomi from https://github.com/Stone-Trees/hardware_xiaomi.git
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-git clone --depth=1 https://github.com/Stone-Trees/device_xiaomi_sm6375-common.git -b lineage-21 device/xiaomi/sm6375-common
+git clone https://github.com/Stone-Trees/device_xiaomi_sm6375-common.git -b lineage-21 device/xiaomi/sm6375-common
 
 git clone --depth=1 https://github.com/Stone-Trees/vendor_xiaomi.git -b lineage-21 vendor/xiaomi
 
@@ -6,6 +6,6 @@ git clone --depth=1 https://github.com/Stone-Trees/kernel_xiaomi_sm6375.git -b l
 
 git clone --depth=1 https://github.com/Stone-Trees/xiaomi_stone-kernel.git device/xiaomi/stone-kernel
 
-git clone --depth=1 https://github.com/stone-devs/hardware_xiaomi hardware/xiaomi
+git clone --depth=1 https://github.com/Stone-Trees/hardware_xiaomi.git hardware/xiaomi
 
 git clone --depth=1 https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
