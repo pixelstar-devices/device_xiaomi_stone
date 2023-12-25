4b1a1a7a24fa92d68322ed72859060c321c9fd9e
kamikaonashi <thorschi93@gmx.at>
2023-12-25 09:43:21 +0100

    stone: make prebuilt kernel optional
    in preparation for oss kernel

--- lineage_stone.mk
+++ lineage_stone.mk
@@ -23,6 +23,9 @@ TARGET_DISABLE_EPPE := true
 # Charging Animation
 TARGET_USE_PIXEL_CHARGER := true
 
+# Use Prebuilt Kernel
+PREBUILT_KERNEL := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_stone
 PRODUCT_DEVICE := stone
