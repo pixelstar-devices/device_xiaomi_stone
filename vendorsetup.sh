18d8b15f952498cafb41383785081aa099ea17d1
Amrito1 <bhabanidas431@gmail.com>
2024-01-23 10:49:39 +0530

    stone: Track Dependencies From My Fork Repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-git clone https://github.com/Stone-Trees/device_xiaomi_sm6375-common.git -b lineage-21 device/xiaomi/sm6375-common
+git clone https://github.com/Amrito-Projects/device_xiaomi_sm6375-common.git -b lineage-21 device/xiaomi/sm6375-common
 
 git clone --depth=1 https://github.com/Stone-Trees/vendor_xiaomi.git -b lineage-21 vendor/xiaomi
 
 git clone --depth=1 https://github.com/Stone-Trees/kernel_xiaomi_sm6375.git -b lineage-20 kernel/xiaomi/sm6375
 
-git clone --depth=1 https://github.com/Stone-Trees/xiaomi_stone-kernel.git device/xiaomi/stone-kernel
+git clone --depth=1 https://github.com/Amrito-Projects/device_xiaomi_stone-kernel.git device/xiaomi/stone-kernel
 
 git clone --depth=1 https://github.com/Stone-Trees/hardware_xiaomi.git hardware/xiaomi
 
