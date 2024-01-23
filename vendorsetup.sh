77c43a2c8887f0affc1cb157f7c41cb92de23430
Amrito1 <bhabanidas431@gmail.com>
2024-01-23 14:24:56 +0530

    stone: Track Updated Prebuilt Kernel
    * Dropped Common Kernel Dependencies

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,9 +2,7 @@ git clone https://github.com/Amrito-Projects/device_xiaomi_sm6375-common.git -b 
 
 git clone --depth=1 https://github.com/Stone-Trees/vendor_xiaomi.git -b lineage-21 vendor/xiaomi
 
-git clone --depth=1 https://github.com/Stone-Trees/kernel_xiaomi_sm6375.git -b lineage-20 kernel/xiaomi/sm6375
-
-git clone --depth=1 https://github.com/Amrito-Projects/device_xiaomi_stone-kernel.git device/xiaomi/stone-kernel
+git clone --depth=1 https://github.com/Amrito-Projects/device_xiaomi_stone-kernel.git -b 13-p device/xiaomi/stone-kernel
 
 git clone --depth=1 https://github.com/Stone-Trees/hardware_xiaomi.git hardware/xiaomi
 
