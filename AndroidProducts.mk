1c3a1cb5580f6b77c78ef24662d865f321687174
Taki <bhabanidas431@gmail.com>
2024-02-05 17:47:37 +0530

    stone: Welcome Pixelstar !
    Co-Authored-By: Mudit <121077424+mudit200408@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_stone.mk
+    $(LOCAL_DIR)/pixelstar_stone.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_stone-user \
-    lineage_stone-userdebug \
-    lineage_stone-eng
+    pixelstar_stone-user \
+    pixelstar_stone-userdebug \
+    pixelstar_stone-eng
