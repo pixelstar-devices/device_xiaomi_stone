2397933ef634f360ef597c7d00dff87806f7bb74
kamikaonashi <thorschi93@gmx.at>
2024-02-25 11:55:35 +0530

    stone: switch to libinit veriant separation
    switch to libinit for device recognition, taken and modified from
    
    https://github.com/Evolution-X-Devices/device_xiaomi_marble/commits/udc/libinit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,5 +21,9 @@ TARGET_OTA_ASSERT_DEVICE := moonstone,sunstone,stone
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_stone
+TARGET_RECOVERY_DEVICE_MODULES := init_stone
+
 # Inherit from the proprietary version
 include vendor/xiaomi/stone/BoardConfigVendor.mk
